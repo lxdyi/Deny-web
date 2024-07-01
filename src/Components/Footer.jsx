c2df61ecf6936d177c419006e42df999d7c5e568
Ziad <ziadzzamel@gmail.com>
2024-07-01 04:13:32 +0300

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,23 @@
 const Footer = () => {
   return (
-    <div className=" bg-[#03AA77] py-5 px-32">
-      <div className="flex items-center justify-between max-w-screen-xl mx-auto">
-        <p className="text-14 text-white">© 2023 Persis Software Labs, All Rights Reserved.</p>
+    <div className=" bg-[#03AA77] py-5 sm:px-10 md:px-16 lg:px-24 xl:px-32">
+      <div className="flex items-center flex-col sm:flex-row gap-5 justify-between max-w-screen-xl mx-auto">
+        <p className="text-[14px] sm:text-[16px]  text-white">
+          © 2024 LXDYI Software , All Rights Reserved.
+        </p>
         <ul className="flex items-center gap-3">
-          <li><img src="/src/assets/linkedin.svg" alt="icon" /></li>
-          <li><img src="/src/assets/facebook.svg" alt="icon" /></li>
-          <li><img src="/src/assets/insta.svg" alt="icon" /></li>
-          <li><img src="/src/assets/X.svg" alt="icon" /></li>
+          <li>
+            <img src="/src/assets/linkedin.svg" alt="icon" />
+          </li>
+          <li>
+            <img src="/src/assets/facebook.svg" alt="icon" />
+          </li>
+          <li>
+            <img src="/src/assets/insta.svg" alt="icon" />
+          </li>
+          <li>
+            <img src="/src/assets/X.svg" alt="icon" />
+          </li>
         </ul>
       </div>
     </div>
